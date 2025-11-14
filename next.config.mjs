3db755c01335c41c52d934fd33ca9b4ffd9fe4c4
shaynelakshitha91-hue <shaynelakshitha91@gmail.com>
2025-11-14 16:26:12 +0530

    Remove ESLint and TypeScript build options

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  output: 'export',
+};
 
-export default nextConfig
+export default nextConfig;
